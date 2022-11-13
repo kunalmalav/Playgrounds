18767107aad957d3674eeb34365a5776088eccf1
kunalmalav <contact@kunalmalav.com>
2022-11-13 15:17:00 +0530

    Arrays

--- swift.playground/Contents.swift
+++ swift.playground/Contents.swift
@@ -1,5 +1,28 @@
 import Cocoa
 
-let celsius:Double = 25.9
-let Fahrenheit:Double = celsius * 9 / 5 + 24
-print("Fahrenheit is = \(Fahrenheit)°F")
+var integerss = Array<Int>()
+integerss.append(45)
+integerss.append(556)
+integerss.append(458)
+print(integerss)
+
+var cities = [String]()
+cities.append("Kota")
+cities.append("Jaipur")
+cities.append("Patna")
+print(cities)
+
+var names = ["kunal", "Kinshu", "Keshav", "kl"]
+print(names.count)
+
+names.remove(at: 3)
+print(names.count)
+print(names)
+
+names.removeAll()
+print(names.count)
+
+var name = ["excuses", "Desires", "Elephant"]
+print(name.sorted())
+
+print(name.reversed())
