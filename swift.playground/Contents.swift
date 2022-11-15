9e41a74cb71e7ccac89f64c024cd119f9a1b6a8a
kunalmalav <contact@kunalmalav.com>
2022-11-15 12:06:08 +0530

    useof default

--- swift.playground/Contents.swift
+++ swift.playground/Contents.swift
@@ -1,5 +1,5 @@
 import Cocoa
- 
+/*
 //var employeee = ["Kunal", "Developer", "Apple"]
 //print("Name: \(employeee[0]")
 //print("Job Title: \(employeee[1])")
@@ -26,3 +26,27 @@ var location = [String: String]()
 location["CA"] = "Baran"
 location["Reso"] = "Kota"
 print(location)
+*/
+
+var employee = ["Name": "Kunal", "Job": "Developer",
+                "Company": "Apple"]
+
+var employeeAge = ["Pandey": 20,
+                   "Malav": 80,
+                   "Pratap": 20]
+
+struct AnimalProperty {
+    var name: String
+    var category: String
+}
+
+var animalDictionary: [Int: AnimalProperty]
+animalDictionary = [1: AnimalProperty(name: "Cow", category: "Domestic"),
+                    2: AnimalProperty(name: "Lion", category: "Wild"),
+                    3: AnimalProperty(name: "Dog", category: "Can be both")]
+
+
+//print(employee["Name", default: <#T##String#>])
+print(employeeAge["Pandey", default: 18])
+print(animalDictionary[4, default: AnimalProperty(name: "Cat", category: "Indoor")])
+
