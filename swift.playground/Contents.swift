6986ff35342f8644b769e2e18bfdb6265584cc33
kunalmalav <contact@kunalmalav.com>
2022-11-17 19:11:33 +0530

    Fizzbuzz

--- swift.playground/Contents.swift
+++ swift.playground/Contents.swift
@@ -1,52 +1,16 @@
 import Cocoa
-/*
-//var employeee = ["Kunal", "Developer", "Apple"]
-//print("Name: \(employeee[0]")
-//print("Job Title: \(employeee[1])")
-//print("company: \(employeee[2])")
-
-var employee = ["Name": "Kunal", "Job": "Developer",
-                "Company": "Apple"]
-print(employee["Name", default: "unknown"])
-print(employee["Job", default: "unknown"])
-print(employee["Company", default: "unkown"])
-
-var schools = [
-    2010: "central academy",
-    2018: "resonance"]
-
-print(schools[2018, default: "unknown"])
-
-var school = [String: Int]()
-school["CA"] = 2010
-school["Reso"] = 2018
-print(school)
-
-var location = [String: String]()
-location["CA"] = "Baran"
-location["Reso"] = "Kota"
-print(location)
-*/
-
-var employee = ["Name": "Kunal", "Job": "Developer",
-                "Company": "Apple"]
-
-var employeeAge = ["Pandey": 20,
-                   "Malav": 80,
-                   "Pratap": 20]
-
-struct AnimalProperty {
-    var name: String
-    var category: String
+var fizzbuzz = [Int]()
+for i in 1...100{
+    if i.isMultiple(of: 3){
+        print(" \(i) Fizz ")
+    }
+    else if i.isMultiple(of: 5){
+        print("\(i) Buzz")
+    }
+    else if i%3==0 && i%3==0{
+        print("\(i) FIzzBuzz")
+    }
+    else{
+        print("\(i)")
+    }
 }
-
-var animalDictionary: [Int: AnimalProperty]
-animalDictionary = [1: AnimalProperty(name: "Cow", category: "Domestic"),
-                    2: AnimalProperty(name: "Lion", category: "Wild"),
-                    3: AnimalProperty(name: "Dog", category: "Can be both")]
-
-
-//print(employee["Name", default: <#T##String#>])
-print(employeeAge["Pandey", default:22])
-print(animalDictionary[4, default: AnimalProperty(name: "Cat", category: "Indoor")])
-
